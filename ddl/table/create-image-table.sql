8110eee6367bf30ea37c1028e066aac74a24a00a
takasumi-m <takasumi.mishima@gmail.com>
2024-10-07 13:34:30 +0900

    update: create-image-table.sql

--- ddl/table/create-image-table.sql
+++ ddl/table/create-image-table.sql
@@ -1,5 +1,7 @@
 drop table if exists gallery.image;
 CREATE TABLE gallery.image (
     image_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    s3_url VARCHAR(255) NOT NULL
+    file_path VARCHAR(255) NOT NULL,
+    unique_filename VARCHAR(255) NOT NULL UNIQUE,
+    original_filename VARCHAR(255) NOT NULL
 );
