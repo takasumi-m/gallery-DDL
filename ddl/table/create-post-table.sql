6af345b9a8dcb3c95e93516b43598be8920270a7
takasumi-m <takasumi.mishima@gmail.com>
2024-10-08 18:48:09 +0900

    update: create-image-table.sql

--- ddl/table/create-post-table.sql
+++ ddl/table/create-post-table.sql
@@ -4,9 +4,10 @@ CREATE TABLE gallery.post (
     status VARCHAR(20) NOT NULL,
     caption TEXT,
     location VARCHAR(255),
-    upload_date DATE NOT NULL,
-    upload_datetime DATETIME NOT NULL,
+    post_date DATE NOT NULL,
+    post_datetime DATETIME NOT NULL,
     delete_password VARCHAR(255) NOT NULL,
     FULLTEXT INDEX idx_fulltext_post_01 (caption),
-    INDEX idx_post_01 (upload_datetime)
+    INDEX idx_post_01 (post_date),
+    INDEX idx_post_02 (post_datetime)
 );
